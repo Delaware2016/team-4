fcdb6d0c516fd1abbd2cdfbd9d4a595a31660a45
zfais74 <ifaison@udel.edu>
2016-10-08 06:24:37 -0400

    added code testing out calendar events

--- framecalendar.js
+++ framecalendar.js
@@ -1,8 +1,32 @@
+//makes the calendar apppear
 $('#calendar-container').datepicker({
   inline: true,
   firstDay: 1,
   showOtherMonts: true,
   dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-
-
 });
+
+$('a').clicked(function(){
+  document.write(Events[0].name);
+});
+
+var chickenBeer = {
+ name: "Chicken Beer 2016",
+ location: "880 Power mill rd",
+ uid: 1846,
+ pplAttended: 1234,
+ date: $('#calendar-container').datepicker("getDate"),
+ type: "Bar"
+};
+
+
+var dankMemes = {
+  name: "Dankest",
+  location: "383 new linden hill rd",
+  udi: 2625,
+  pplAttended: 3652,
+  date: $('#calendar-container').datepicker("getDate"),
+  type: "Funny"
+};
+
+var Events = [chickenBeer, dankMemes]
