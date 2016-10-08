6dea1e9b565454d15e72a444a467df45db27ff18
AJM <mrpeanut293@slippeddisk.net>
2016-10-08 03:48:53 -0400

    #MakeCodeGreatAgain as I construct an indecipherable wall of php

--- logon.php
+++ logon.php
@@ -1,42 +1,27 @@
-<!DOCTYPE html>
-<html>
-<title>FOMO Login</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
+  require_once("support.php");
 
-<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+  $footer = "";
 
-<style>
-body,h1 {font-family: "Raleway", sans-serif}
-body, html {height: 100%}
-.bgimg-1 {
-    background-image: url('delaware.jpg');
-    min-height: 100%;
-    background-position: center;
-    background-size: cover;
-}
-</style>
-<body>
-
-<div class="bgimg-1 w3-display-container w3-animate-opacity w3-text-white">
-  <div class="w3-display-topleft w3-padding-large w3-xlarge">
-    <img src="fomo_logo.png" alt="FOMO - DE" style="width:100px; height: 100px;">
-  </div>
-  <div style="background-color: rgba(0,0,0,0.75); border-radius: 25px; padding-left: 20px; padding-right: 20px; padding-bottom: 20px; text-align:center" class="w3-display-middle">
-    <h1 class="w3-jumbo w3-animate-top">FOMO LOGIN</h1>
+  $body = <<<EOBODY
+  <div class="bgimg-1 w3-display-container w3-animate-opacity w3-text-white">
+    <div class="w3-display-topleft w3-padding-large w3-xlarge">
+      <img src="fomo_logo.png" alt="FOMO - DE" style="width:100px; height: 100px;">
+    </div>
+    <div style="background-color: rgba(0,0,0,0.75); border-radius: 25px; padding-left: 20px; padding-right: 20px; padding-bottom: 20px; text-align:center" class="w3-display-middle">
+      <h1 class="w3-jumbo w3-animate-top">FOMO LOGIN</h1>
     <hr/>
-		<div id = "login_style">
-			
-			<p> Logging into FOMO gives you access to the best Delaware has to offer! </p>
-			<input type = "text" id = "login" placeholder = "Email Id" name = "uid">
-   			<input type = "password" id = "password" name = "upass" placeholder = "***">
-   			<input type = "submit" id = "dologin" value = "Login" onclick="location.href='frame.html';">
-		</div>
-    
+      <div id = "login_style">	
+	<p> Logging into FOMO gives you access to the best Delaware has to offer! </p>
+	<input type = "text" id = "login" placeholder = "Email Id" name = "uid">
+   	<input type = "password" id = "password" name = "upass" placeholder = "***">
+   	<input type = "submit" id = "dologin" value = "Login" onclick="location.href='frame.html';">
+      </div>
+    </div>
   </div>
-	
-  
-</div>
+EOBODY;
 
-</body>
-</html>
+  $page = $body.$footer;
+
+  echo generatePage($page);
+?>
